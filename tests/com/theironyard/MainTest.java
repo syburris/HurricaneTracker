55181022b767da5ecb9997e9fea9b8ed7998a870
Steven Burris <stevenburris.jr@gmail.com>
2016-10-14 13:31:03 -0400

    Working on test methods;

--- tests/com/theironyard/MainTest.java
+++ tests/com/theironyard/MainTest.java
@@ -1,8 +1,11 @@
 package com.theironyard;
 
+import org.junit.Test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -14,6 +17,36 @@ public class MainTest {
         Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
         Main.createTables(conn);
         return conn;
+    }
+
+    @Test
+    public void testUser() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertUser(conn,"Steven","pass");
+        User user = Main.selectUser(conn,"Steven");
+        conn.close();
+        assertTrue(user != null);
+    }
+
+    @Test
+    public void testHurricane() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertHurricane(conn, "Matt", "Chuck", 2, "www.google.com", "Steven");
+        Main.insertHurricane(conn, "Floyd", "NC", 3, "www.google.com", "Andy");
+        Main.insertHurricane(conn, "Katrina", "Nola", 3, "www.google.com", "Sam");
+        ArrayList<Hurricane> hurricanes = Main.selectHurricanes(conn,"","");
+        assertTrue(!hurricanes.isEmpty());
+    }
+
+    @Test
+    public void testDeleteHurricane() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertHurricane(conn, "Matt", "Chuck", 2, "www.google.com", "Steven");
+        Main.insertHurricane(conn, "Floyd", "NC", 3, "www.google.com", "Andy");
+        Main.insertHurricane(conn, "Katrina", "Nola", 3, "www.google.com", "Sam");
+        ArrayList<Hurricane> hurricanes = Main.selectHurricanes(conn,"","");
+        Main.deleteHurricane(conn,1);
+        assertTrue(hurricanes.size() == 2);
     }
 
 }
